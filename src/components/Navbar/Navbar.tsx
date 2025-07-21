4933e839f9e7caaf09125f995d739b4d29c7d0c1
Ryan Mark <mark_dev@my.pisopay.com.ph>
2025-07-21 14:15:36 +0800

    add burger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,16 +4,24 @@ import Logo from "@/assets/images/hotel-logo.svg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@heroui/react";
+import { Button, divider, toggle } from "@heroui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  FaFacebook,
+  FaInstagramSquare,
+  FaTimes,
+  FaYoutube,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const NavItems = () => {
   return (
-    <ul className="flex gap-20">
+    <ul className="flex flex-col gap-12 text-center p-24 pb-14  lg:flex-row lg:gap-20 lg:p-10">
       {Navlinks.map((item) => (
         <li key={item.id}>
           <HashLink
             to={item.href}
-            className=" text-white hover:text-gray-300 transition duration-300 ease-in-out "
+            className=" text-black font-semibold lg:text-white"
           >
             {item.title}
           </HashLink>
@@ -23,7 +31,7 @@ const NavItems = () => {
   );
 };
 
-const Navbar = () => {
+const BookButton = () => {
   const [user, setUser] = useState<any>({});
   const [userToken, setUserToken] = useState<string | null>(null);
   const navigate = useNavigate();
@@ -41,7 +49,6 @@ const Navbar = () => {
           },
         });
         setUser(response.data.data);
-        console.log(response.data.data);
       } else {
         return;
       }
@@ -60,25 +67,74 @@ const Navbar = () => {
   const fullName = `${user?.firstname ?? ""} ${user?.lastname ?? ""}`;
 
   return (
-    <header className="max-w-full flex justify-between items-center py-8 px-12">
+    <>
+      {userToken ? (
+        <div className="text-white w-24">
+          {fullName ?? <div className="w-full"></div>}
+        </div>
+      ) : (
+        <Button
+          onPress={() => navigate("/login")}
+          className="bg-yuma-300 px-10 w-64 lg:w-36"
+          radius="sm"
+        >
+          <span className="text-white">Book Now</span>
+        </Button>
+      )}
+    </>
+  );
+};
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="max-w-full flex justify-between items-center py-8 px-8 lg:px-12">
+      {/* Desktop View */}
       <img src={Logo} alt="Logo" className="w-24" />
-      <nav>
+      <nav className="hidden lg:flex">
         <NavItems />
       </nav>
-      <div className="">
-        {userToken ? (
-          <div className="text-white w-24">
-            {fullName ?? <div className="w-full"></div>}
-          </div>
-        ) : (
-          <Button
-            onPress={() => navigate("/login")}
-            className="bg-yuma-300 px-10"
-            radius="sm"
-          >
-            <span className="text-white">Book Now</span>
-          </Button>
-        )}
+
+      {/* Mobile View */}
+      <div className="text-white lg:hidden">
+        <div>
+          <button onClick={toggleMenu}>
+            {!isOpen && <GiHamburgerMenu size={20} />}
+          </button>
+
+          {isOpen && (
+            <div className="fixed bg-white top-0 left-0 w-screen h-screen overflow-hidden z-30 lg:hidden">
+              <div className=" flex justify-between py-8 px-8">
+                <img src={Logo} alt="Logo" className="w-10" />
+                <h1 className="text-2xl text-black font-bold">INFINITY</h1>
+                <button onClick={toggleMenu}>
+                  {isOpen && <FaTimes size={20} className="text-yuma-500" />}
+                </button>
+              </div>
+              <div className="flex flex-col">
+                <NavItems />
+              </div>
+              <div className="text-black flex justify-center gap-4 p-5">
+                <FaFacebook size={20} />
+                <FaInstagramSquare size={20} />
+                <FaXTwitter size={20} />
+                <FaYoutube size={20} />
+              </div>
+              <div className="flex justify-center">
+                <BookButton />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="hidden lg:flex">
+        <BookButton />
       </div>
     </header>
   );
--- src/components/Navbar/Navlinks.ts
+++ src/components/Navbar/Navlinks.ts
@@ -1,22 +1,22 @@
-export const Navlinks = [ 
-    {
-        id: 1, 
-        title: 'Home',
-        href: '#home'
-    },
-    {
-        id: 2, 
-        title: 'Facilities and Amenities',
-        href: '#facilities'
-    },
-    {
-        id: 3, 
-        title: 'Dining',
-        href: '#dining'
-    },
-    {
-        id: 4, 
-        title: 'Contacts',
-        href: '#contacts'
-    }
+export const Navlinks = [
+  {
+    id: 1,
+    title: "HOME",
+    href: "#home",
+  },
+  {
+    id: 2,
+    title: "FACILITIES & AMENITIES",
+    href: "#facilities",
+  },
+  {
+    id: 3,
+    title: "DINING",
+    href: "#dining",
+  },
+  {
+    id: 4,
+    title: "CONTACTS",
+    href: "#contacts",
+  },
 ];
