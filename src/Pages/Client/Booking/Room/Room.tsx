194e2fe2ea691febebbec2ec27288bfacb104862
Ryan Mark <mark_dev@my.pisopay.com.ph>
2025-11-03 16:42:15 +0800

    fix error: remove unuse package

--- src/Pages/Client/Booking/Room/Room.tsx
+++ src/Pages/Client/Booking/Room/Room.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Card,
   CardHeader,
--- src/Pages/Client/Home/Home.tsx
+++ src/Pages/Client/Home/Home.tsx
@@ -3,7 +3,6 @@ import About from "./About";
 import Location from "./Location";
 import Room from "./Room&Suites";
 import Services from "./Facilities&Services";
-import Footer from "../../../components/Footer";
 import RoomTypeContextProvider from "../../../contexts/roomTypeContext";
 
 const Home = () => {
